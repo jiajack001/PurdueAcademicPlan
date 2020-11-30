abfaf159e286239684d3da2dc296738a7707a825
Jack Jia <jiajack001@gmail.com>
2020-11-29 20:41:14 -0500

    Initial Commit

---
+++ app/src/main/java/ldp/e/purdueacademicplan/PlanOverview.java
@@ -0,0 +1,13 @@
+package ldp.e.purdueacademicplan;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlanOverview extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plan_overview);
+
+    }
+}
---
+++ app/src/main/java/ldp/e/purdueacademicplan/StudentProfile.java
@@ -0,0 +1,14 @@
+package ldp.e.purdueacademicplan;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StudentProfile extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_profile);
+
+
+    }
+}
