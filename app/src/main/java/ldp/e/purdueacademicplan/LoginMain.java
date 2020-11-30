980d1ac400a1b4e1c22bd9a5cd75826fcdfd543b
Jack Jia <jiajack001@gmail.com>
2020-11-29 19:35:58 -0500

    Inital Commit

--- app/src/main/java/ldp/e/purdueacademicplan/LoginMain.java
+++ app/src/main/java/ldp/e/purdueacademicplan/LoginMain.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+
 
 public class LoginMain extends AppCompatActivity {
     EditText usernameEditText;
@@ -24,7 +30,22 @@ public class LoginMain extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
+                // Password Hash using SHA-256
+                String password = passwordEditText.getText().toString();
+                String hash = "";
+                try {
+                    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                    byte[] hashByte = digest.digest(password.getBytes("UTF-8"));
+
+                    for (byte b : hashByte) {
+                        String hex = String.format("%02x", b);
+                        hash += hex;
+                    }
+
+                }catch(Exception ex) {
+                    ex.printStackTrace();
+                }
             }
         });
 
